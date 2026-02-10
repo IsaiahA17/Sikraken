e7d5b113f03e80b983dcee83ec480d098fcee2d1
IsaiahA17 <nashandres5665@gmail.com>
2026-02-10 16:51:36 +0000

    Adding More Debug Lines

--- bin/test_category_sikraken_ecs.sh
+++ bin/test_category_sikraken_ecs.sh
@@ -192,7 +192,8 @@ for entry in "${ALL_BENCHMARKS[@]}"; do
     if [[ $SIKRAKEN_EXIT -ne 0 ]]; then
         echo "WARNING: Sikraken exited with code $SIKRAKEN_EXIT for $BENCH"
         find "$SIKRAKEN_ROOT/shared/${BENCH#/shared/}"
-        ls /pthread-ext
+        find /app/shared/${BENCH#/shared/}"
+        pwd
     else
         echo "Sikraken exited successfully for $BENCH"
     fi
